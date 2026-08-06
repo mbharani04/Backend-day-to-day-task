88bfbd7b1ba766e6fc8d8990461ee6cb6e272f5b
mbharani04 <166915389+mbharani04@users.noreply.github.com>
2026-08-06 16:52:36 +0530

    updated

---
+++ node/day13/server/middleware/authMiddle.js
@@ -0,0 +1,14 @@
+
+
+
+
+export const authMiddleware = (req,res,next)=>{
+
+     try {
+        const authHeaders = req.headers.authorization 
+        
+        
+     } catch (error) {
+        
+     }     
+}
